5fbba49e9f14dfe339efc0eeec3d2b447d35df35
Anna <michalowska.aneczka@gmail.com>
2023-03-11 17:29:56 +0100

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -2,11 +2,22 @@ import { createSlice } from '@reduxjs/toolkit';
 import { createAction } from '@reduxjs/toolkit';
 import { fetchContacts, addContact, deleteContact } from './operations';
 
+const handlePending = state => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState: {
     contacts: [],
     filter: '',
+    loading: false,
+    error: null
   },
   reducers: {
     filteredContact(state, action) {
@@ -14,13 +25,25 @@ const contactsSlice = createSlice({
     },
   },
   extraReducers: {
+    [fetchContacts.pending]: handlePending,
+    [addContact.pending]: handlePending,
+    [deleteContact.pending]: handlePending,
+    [fetchContacts.rejected]: handleRejected,
+    [addContact.rejected]: handleRejected,
+    [deleteContact.rejected]: handleRejected,
     [fetchContacts.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.error = null;
       state.contacts = action.payload;
     },
     [addContact.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.error = null;
       state.contacts.push(action.payload);
     },
     [deleteContact.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.error = null;
       state.contacts = state.contacts.filter(
         contact => contact.id !== action.payload
       );
