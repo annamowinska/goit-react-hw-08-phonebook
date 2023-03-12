a362940593910c80204511f50aa0487c1b1f0b41
Anna <michalowska.aneczka@gmail.com>
2023-03-12 18:17:49 +0100

    last_today

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,37 @@
+import backgroundImage from '../image/background.jpg';
+
 const styles = {
   container: {
     minHeight: 'calc(100vh - 50px)',
     display: 'flex',
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
   title: {
-    fontWeight: 500,
+    fontWeight: 700,
     fontSize: 38,
     textAlign: 'center',
-    color: '#f2e9e4',
+    color: '#c9ada7',
+  },
+
+  background: {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    minHeight: '80vh',
+    width: '100%',
+    marginTop: '10px',
   },
 };
 
 export default function Home() {
   return (
     <div style={styles.container}>
-      <h1 style={styles.title}>
-        Contact manager welcome page{' '}
-        <span role="img" aria-label="Greeting icon">
-          💁‍♀️
-        </span>
-      </h1>
+      <h1 style={styles.title}>Contact manager welcome page</h1>
+      <div style={styles.background}></div>
     </div>
   );
 }
